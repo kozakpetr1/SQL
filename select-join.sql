42480019b2bddac8328cf973f2110b817d098c58
petr@vachasoftware.com <petr@vachasoftware.com>
2022-04-27 11:28:43 +0200

    SQL

--- select-join.sql
+++ select-join.sql
@@ -43,3 +43,5 @@ FROM `plang`.`lang`, `plang`.`author`
 INNER JOIN `plang`.`createdby`
 WHERE `createdby`.`lang_idlang` = `lang`.`idlang`
 AND `createdby`.`author_idauthor` = `author`.`idauthor`;
+
+SELECT COUNT(DISTINCT `lang`.`designation`) FROM `plang`.`lang`;
