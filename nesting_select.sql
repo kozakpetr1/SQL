19b6d939957ce57433be35971583fd3f1d655673
petr@vachasoftware.com <petr@vachasoftware.com>
2023-03-01 10:59:10 +0100

    SQL essentials

---
+++ nesting_select.sql
@@ -0,0 +1,48 @@
+# VNOŘOVÁNÍ DOTAZŮ
+
+# Dotaz s vnořeným dotazem vrátí seznam všech autorů
+# jazyka C (lang_idlang = 26). Vnořený dotaz (vnitřní)
+# je proveden jako první. Jeho výstup je použit v dotazu
+# vnějším.
+# Za klauzulí IN je množina hodnot identifikátorů
+# autorů, např. (1, 7, 25, 36).
+# Klauzule IN nahrazuje zápis pomocí OR, který by byl mnohem delší.
+
+SELECT 
+    CONCAT(UPPER(surname), " ", firstname)
+FROM
+    plang.author
+WHERE
+    idauthor IN (SELECT 
+            author_idauthor
+        FROM
+            plang.createdby
+        WHERE
+            lang_idlang = 26);
+
+# Dotaz s vnořeným dotazem vrátí seznam všech autorů,
+# kteří nemají v databázi přiřazen žádný programovací jazyk,
+# jehož jsou autory.
+
+SELECT 
+    idauthor, firstname, surname
+FROM
+    plang.author
+WHERE
+    idauthor NOT IN (SELECT author_idauthor FROM createdby);
+
+# Dotaz vrátí statistické údaje o počtu autorů na jazyk.
+# Vnitřní dotaz je uvnitř klauzule FROM, což povede
+# k vytvoření dočasné tabulky items, ze které je poté
+# proveden vnější dotaz.
+
+SELECT 
+    MAX(items) AS max_authors_per_lang,
+    MIN(items) AS min_authors_per_lang,
+    CONVERT(AVG(items),DECIMAL(10,2)) AS avg_authors_per_lang
+FROM
+    (SELECT 
+        lang_idlang, COUNT(lang_idlang) AS items
+    FROM
+        plang.createdby
+    GROUP BY lang_idlang) AS amount;
--- select-join.sql
+++ select-join.sql
@@ -1,47 +1,77 @@
-SELECT *
-FROM `plang`.`lang`
-JOIN `plang`.`createdby`
-ON `lang`.`idlang` = `createdby`.`lang_idlang`;
+# Propojení dvou tabulek pomocí klauzule JOIN (INNER JOIN).
+# Dotaz vrátí jazyky, jejich autory a roky vzniku.
+
+SELECT 
+    lang.designation AS programming_language,
+    CONCAT(UPPER(surname), " ", firstname) AS author,
+    lang.created
+FROM 
+    plang.lang, plang.author
+INNER JOIN 
+    plang.createdby
+WHERE
+    lang.idlang = createdby.lang_idlang
+    AND author.idauthor = createdby.author_idauthor;
 
 SELECT *
-FROM `plang`.`lang`
-INNER JOIN `plang`.`createdby`
-ON `lang`.`idlang` = `createdby`.`lang_idlang`;
+FROM plang.lang
+INNER JOIN plang.createdby
+ON lang.idlang = createdby.lang_idlang;
 
 SELECT *
-FROM `plang`.`lang`
-LEFT JOIN `plang`.`createdby`
-ON `lang`.`idlang` = `createdby`.`lang_idlang`;
+FROM plang.lang, plang.createdby
+WHERE lang.idlang = createdby.lang_idlang;
 
 SELECT *
-FROM `plang`.`lang`
-RIGHT JOIN `plang`.`createdby`
-ON `lang`.`idlang` = `createdby`.`lang_idlang`;
+FROM plang.lang
+LEFT JOIN plang.createdby
+ON lang.idlang = createdby.lang_idlang;
 
 SELECT *
-FROM `plang`.`lang`
-CROSS JOIN `plang`.`createdby`;
+FROM plang.lang
+LEFT JOIN plang.createdby
+ON lang.idlang = createdby.lang_idlang
+WHERE createdby.lang_idlang IS NULL;
 
 SELECT *
-FROM `plang`.`lang`, `plang`.`createdby`;
+FROM plang.lang
+RIGHT JOIN plang.createdby
+ON lang.idlang = createdby.lang_idlang;
 
-SELECT * FROM `plang`.`lang` WHERE `lang`.`idlang` BETWEEN 5 AND 7
+SELECT *
+FROM plang.lang
+RIGHT JOIN plang.createdby
+ON lang.idlang = createdby.lang_idlang
+WHERE lang.idlang IS NULL;
+
+SELECT *
+FROM plang.lang
+CROSS JOIN plang.createdby;
+
+SELECT *
+FROM plang.lang, plang.createdby;
+
+SELECT * FROM plang.lang WHERE lang.idlang BETWEEN 5 AND 7
 UNION
-SELECT * FROM `plang`.`lang` WHERE `lang`.`idlang` BETWEEN 6 AND 8;
+SELECT * FROM plang.lang WHERE lang.idlang BETWEEN 6 AND 8;
 
-SELECT * FROM `plang`.`lang` WHERE `lang`.`idlang` BETWEEN 5 AND 7
+SELECT * FROM plang.lang WHERE lang.idlang BETWEEN 5 AND 7
 UNION ALL
-SELECT * FROM `plang`.`lang` WHERE `lang`.`idlang` BETWEEN 6 AND 8;
+SELECT * FROM plang.lang WHERE lang.idlang BETWEEN 6 AND 8;
 
-SELECT * FROM `plang`.`lang`, `plang`.`author`
-INNER JOIN `plang`.`createdby`
-WHERE `createdby`.`lang_idlang` = `lang`.`idlang`
-AND `createdby`.`author_idauthor` = `author`.`idauthor`;
+SELECT * FROM plang.lang, plang.author
+INNER JOIN plang.createdby
+WHERE createdby.lang_idlang = lang.idlang
+AND createdby.author_idauthor = author.idauthor;
 
-SELECT `lang`.`designation`, `author`.`firstname`, `author`.`surname`
-FROM `plang`.`lang`, `plang`.`author`
-INNER JOIN `plang`.`createdby`
-WHERE `createdby`.`lang_idlang` = `lang`.`idlang`
-AND `createdby`.`author_idauthor` = `author`.`idauthor`;
+SELECT lang.designation, author.firstname, author.surname
+FROM plang.lang, plang.author
+INNER JOIN plang.createdby
+WHERE createdby.lang_idlang = lang.idlang
+AND createdby.author_idauthor = author.idauthor;
 
-SELECT COUNT(DISTINCT `lang`.`designation`) FROM `plang`.`lang`;
+# Výběr počtu hodnot bez klauzule DISTINCT.
+SELECT COUNT(createdby.lang_idlang) FROM plang.createdby;
+
+# Výběr počtu neopakujících (různých) se hodnot pomocí klauzule DISTINCT.
+SELECT COUNT(DISTINCT createdby.lang_idlang) FROM plang.createdby;
