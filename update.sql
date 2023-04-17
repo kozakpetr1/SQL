ba54a35d2fe34f09df05eb5db8d6df63f28fa21d
petr@vachasoftware.com <petr@vachasoftware.com>
2023-04-17 10:47:00 +0200

    SQL

--- update.sql
+++ update.sql
@@ -5,6 +5,12 @@ UPDATE `plang`.`author`
 SET company = 'Bell Labs'
 WHERE surname = 'Kernighan' AND firstname = 'Brian';
 
-UPDATE `plang`.`author`
+UPDATE plang.author
 SET company = 'Bell Labs'
 WHERE surname = 'Ritchie' AND firstname = 'Dennis';
+
+UPDATE plang.author
+SET surname = UPPER(surname);
+
+UPDATE plang.author
+SET surname = CONCAT(UPPER(SUBSTRING(surname,1,1)),LOWER(SUBSTRING(surname,2)));
