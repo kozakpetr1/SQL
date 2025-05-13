cbcd2deb1787009b99b358ee756c7a50e0125ff2
Petr Kozák <kozak.petr@gmail.com>
2025-05-13 10:04:47 +0200

    Aktualizace 13.5.2025

---
+++ examples/vehicles/3IT.md
@@ -0,0 +1,5 @@
+#### Proveď výběr (SELEKCE) všech vlastníků vozidel a jejich vozidel (JOIN) se sloupci (PROJEKCE) firstname, lastname, vin_code, engine, model (název modelu), manufacturer_name.
+** Zde bude MD tabulka s výstupem dotazu **
+
+#### Proveď výběr vlastníků vozidel, jejichž id je větší nebo rovno 5.
+** Zde bude MD tabulka s výstupem dotazu. **
---
+++ examples/vehicles/3IT.sql
@@ -0,0 +1,8 @@
+-- Proveď výběr (SELEKCE) všech vlastníků vozidel a jejich vozidel (JOIN) se sloupci (PROJEKCE) firstname, lastname, vin_code, 
+-- engine, model (název modelu), manufacturer_name.
+
+-- Napiš proceduru add_owner, která bude umět přidat nového vlastníka do tabulky owners.
+
+-- Použij proceduru add_owner pro přidání nového vlastníka vozu (údaje si vymysli).
+
+-- Proveď výběr vlastníků vozidel, jejichž id je větší nebo rovno 5.
